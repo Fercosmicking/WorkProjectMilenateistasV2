a0433d4fbea34b0b021d68026e1134c17d6d198c
Juan Fer <83317226+Fercosmicking@users.noreply.github.com>
2022-08-11 17:30:23 -0500

    Task 4 done (#3)

--- task4.cpp
+++ task4.cpp
@@ -3,6 +3,39 @@
 
 
 void task4(){
+    enum class Task4States{
+        INIT,
+        WAIT_TO_TOGGLE_LED
+    };
+    static Task4States task4State = Task4States::INIT;
+    static uint32_t lasTime;
+    static constexpr uint32_t INTERVAL = 250;
+    static constexpr uint8_t ledRed = 27;
+    static bool ledStatus = false;
+
+    switch(task4State){
+        case Task4States::INIT:{
+            pinMode(ledRed,OUTPUT);
+            lasTime = millis();
+            task4State = Task4States::WAIT_TO_TOGGLE_LED;
+            break;
+        }
+
+        case Task4States::WAIT_TO_TOGGLE_LED:{
+            // evento 1:
+            uint32_t currentTime = millis();
+            if( (currentTime - lasTime) >= INTERVAL ){
+                lasTime = currentTime;
+                digitalWrite(ledRed,ledStatus);
+                ledStatus = !ledStatus;
+            }
+            break;
+        }
+
+        default:{
+            break;
+        }
+    }
 
     
 }
