71e8a8143b241eb72370ba74d17aad3f3d35320c
Juan Fer <83317226+Fercosmicking@users.noreply.github.com>
2022-08-11 17:00:43 -0500

    Task 1 done (#1)

--- task1.cpp
+++ task1.cpp
@@ -3,6 +3,42 @@
 
 
 void task1(){
+    enum class Task1States{
+        INIT,
+        WAIT_TO_TOGGLE_LED
+    };
+    static Task1States task1State = Task1States::INIT;
+    static uint32_t lasTime;
+    static constexpr uint32_t INTERVAL = 100;
+    static constexpr uint8_t ledRed = 14;
+    static bool ledStatus = false;
+    //Acá vamos a usar es un bool para verificar el estado de LED.
+
+    switch(task1State){
+        case Task1States::INIT:{
+            pinMode(ledRed,OUTPUT);
+            lasTime = millis();
+            task1State = Task1States::WAIT_TO_TOGGLE_LED;
+            break;
+        }
+
+        //Cambios de tiempo entre los ciclos. En uno estará prendido el LED y en el otro, pues, no :(
+
+        case Task1States::WAIT_TO_TOGGLE_LED:{
+            // evento 1:
+            uint32_t currentTime = millis();
+            if( (currentTime - lasTime) >= INTERVAL ){
+                lasTime = currentTime;
+                digitalWrite(ledRed,ledStatus);
+                ledStatus = !ledStatus;
+            }
+            break;
+        }
+
+        default:{
+            break;
+        }
+    }
 
     
 }
